75ca400722198d3fca17f9851ef71f5132c3274a
Tahmid Rahman <tahmid11q@gmail.com>
2021-07-18 22:49:38 +0600

    Function and Class Components

---
+++ src/components/HelloClass.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class HelloClass extends Component{
+    render(){
+    	return <h1>Hello From Class!</h1>
+    }
+}
+
+export default HelloClass
---
+++ src/components/HelloFunc.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function HelloFunction(){
+	return <h1>Hello From Function!</h1>
+}
+
+export default HelloFunction;
